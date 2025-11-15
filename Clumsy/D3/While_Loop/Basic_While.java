c1a7ec7908d768f5e71ac740c58ba9ba047cc459
Prashant Shukla <prashantshukla83036@gmail.com>
2025-11-15 20:11:27 +0530

    Create Basic_While.java

---
+++ Clumsy/D3/While_Loop/Basic_While.java
@@ -0,0 +1,17 @@
+package Clumsy.D3.While_Loop;
+
+public class Basic_While {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("\n");
+        int i = 1;
+
+        while ( i <= 5) {
+            System.out.println("Count " + i);
+            i++;
+                System.out.println("");
+        }
+        System.out.println("While Loop Finished :)\n ");
+    }
+}
