154c7397daaf909c7316b4ecaded075361bf0dad
Prashant Shukla <67216199+prashant75700@users.noreply.github.com>
2025-11-27 13:53:09 +0530

    Enhance Student and StudentTest classes with constructors, grading logic, and display methods

--- Clumsy/D4/Opps/Std/Student.java
+++ Clumsy/D4/Opps/Std/Student.java
@@ -6,16 +6,69 @@ public class Student {
     int age;
     int rollNo;
     double gpa;
+   //char lettergrade;
+    //public char[] getLettergrade;
+
+    //now we are going to use constructors: 
+
+    Student(String studentName, int studentAge, double studentGpa) {
+        name = studentName;
+        age = studentAge;
+        gpa = studentGpa;
+        //lettergrade = getLettergrade;
+        System.out.println("New Student Created: " + name);
+    }
 
     //methode to display info:
     void displayinfo() {
-        System.out.println("===== STUDENT INFO =====\"");
+        System.out.println("");
+        System.out.println("===== STUDENT INFO =====");
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
         System.out.println("Roll NO: " + rollNo);
         System.out.println("GPA: " + gpa);
+        System.out.println("Grade: " + getLettergrade());
         System.out.println("========================");
-
-    //methode to 
     }
+
+    //methode to cheack if student is pass or fail
+
+    void cheackPass () {
+        if (gpa >= 40.0) {
+            System.out.println( name + " Passed ");
+        }
+        else {
+            System.out.println(name + " Failed ");
+        }
+    
+    }
+
+    char getLettergrade () {
+        if (gpa >= 90.0) {
+           // System.out.println(name + " You got outstanding grade \'A\'");
+            return 'A';
+        }
+        else if (gpa >= 80.0) {
+            //System.out.print(name + " Great buddy You secured a amazing grade: ");
+            return 'B';
+        }
+        else if (gpa >= 70.0) {
+            //System.out.print(name + " Yoohho You just Scored a reliable Grade: ");
+            return 'C';
+        }
+        else if (gpa >= 60.0) {
+            //System.out.print(name  + " YOOOO you got a Decent grade: ");
+            return 'D';
+        }
+        else if (gpa >= 40.0) {
+            //System.out.print(name + " You just got yourself safe this time with the Grade: ");
+            return 'E';
+        }
+        else {
+            //System.out.print(name + " Sh***t dude , u didn't score enough to pass the exam T-T , Grade: ");
+            return 'F';
+        }
+    }
+
+
 }
--- Clumsy/D4/Opps/Std/StudentTest.java
+++ Clumsy/D4/Opps/Std/StudentTest.java
@@ -4,7 +4,7 @@ public class StudentTest {
     
     public static void main(String[] args) {
         
-        Student student1 = new Student();
+        Student student1 = new Student("Abhinav", 0016, 92.6);
         student1.name = "Abhinav";
         student1.age = 16;
         student1.rollNo = 212635;
@@ -14,5 +14,31 @@ public class StudentTest {
         System.out.println(student1.rollNo);
         System.out.println(student1.age);
         System.out.println(student1.gpa);
+        char grade = student1.getLettergrade();
+        System.out.println("Letter Grade " + grade);
+
+        student1.displayinfo();
+        student1.cheackPass();
+        System.out.println("");
+
+        Student student2 = new Student("Saumya", 18, 89.5);
+        Student student3 = new Student("Neelam", 19, 75.2);
+        Student student4 = new Student("Shikhar", 16, 59.8);
+        System.out.println("");
+        student2.rollNo = 125662;
+        student3.rollNo = 364524;
+        student4.rollNo = 425684;
+
+
+
+        student2.displayinfo();
+        student2.cheackPass();
+        student3.displayinfo();
+        student3.cheackPass();
+        student4.displayinfo();
+        student4.cheackPass();
+
+        
+        System.out.println("");
     }
 }
